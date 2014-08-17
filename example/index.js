25985977199513548773bab3e72f1003dd2281bd
xpeper <xpepermint@gmail.com>
2014-08-17 16:54:14 +0200

    doc

--- example/index.js
+++ example/index.js
@@ -9,9 +9,5 @@ app.use(bodyparser()); // required for params to include request body objects
 app.use(params());
 app.use(function *() {
   this.body = this.params.all();
-  // this.body = this.params['id'];
-  // this.body = this.params.only('name', 'age');
-  // this.body = this.params.except('gender');
-  // this.body = this.params.require('user').only('id', 'name');
 });
 app.listen(3001);
